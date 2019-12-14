37b43d7c08c2bf4434d55c8dd1cca75de7fbdcce
facethechallenge69 <54116672+facethechallenge69@users.noreply.github.com>
2019-12-14 10:38:12 -0800

    asdfasdf
    asdfasdf

--- autotest.java
+++ autotest.java
@@ -71,8 +71,8 @@ public class autotest extends LinearOpMode
 
         int encoder_tics = 0;
         double encoder_speed = 0;
-        double last_encode_speed = encoder_speed;
-        int last_encoder_tics = encoder_tics;
+        double last_encode_speed = 7;
+        int last_encoder_tics = 7;
 
         //Setting the behavior for the motors to float.
         motorR_Up.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -105,24 +105,6 @@ public class autotest extends LinearOpMode
         //Waiting for the user to press start
         waitForStart();
 
-        telemetry.addLine("gampad1.a = + 50 encoder tics");
-        telemetry.addLine("gampad1.x = - 50 encoder tics");
-        telemetry.addLine("gampad1.b = + 0.05 encoder speed");
-        telemetry.addLine("gampad1.y = - 0.05 encoder speed");
-
-        telemetry.addLine("gampad1.dpad_up = driveforward");
-        telemetry.addLine("gampad1.dpad_left = turnleft");
-        telemetry.addLine("gampad1.dpad_right = turnright");
-        telemetry.addLine("gampad1.dpad_down = driveforward");
-
-        telemetry.addLine("gampad2.dpad_right = straferight");
-        telemetry.addLine("gampad2.dpad_left = strafeleft");
-        telemetry.addLine("gampad2.dpad_right = turnright");
-        telemetry.addLine("gampad1.dpad_up = armUpDown");
-
-        telemetry.addLine("gampad1.right_bumper = close servo");
-        telemetry.addLine("gampad1.left_bumper = open servo");
-        telemetry.update();
 
         while (opModeIsActive())
         {
@@ -194,11 +176,7 @@ public class autotest extends LinearOpMode
                 last_encode_speed = encoder_speed;
                 telemetry.addData("encoder distance %d", encoder_tics);
                 telemetry.addData("encoder speed %f", encoder_speed);
-<<<<<<< HEAD
-                telemetry.update();
 
-            }
-=======
 
                 telemetry.addLine("gampad1.a = + 50 encoder tics");
                 telemetry.addLine("gampad1.x = - 50 encoder tics");
@@ -224,7 +202,6 @@ public class autotest extends LinearOpMode
 
 
 
->>>>>>> 56177b5a3d37218f63994ed2b13337fe46715833
 
             idle();
         }
