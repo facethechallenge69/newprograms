7718a709f27806f66f28df11f9b59b4e3520cab7
facethechallenge69 <54116672+facethechallenge69@users.noreply.github.com>
2019-12-14 10:57:04 -0800

    adfs
    aasdf

--- autotest.java
+++ autotest.java
@@ -183,15 +183,14 @@ public class autotest extends LinearOpMode
                 telemetry.addLine("gampad1.b = + 0.05 encoder speed");
                 telemetry.addLine("gampad1.y = - 0.05 encoder speed");
 
-                telemetry.addLine("gampad1.dpad_up = driveforward");
-                telemetry.addLine("gampad1.dpad_left = turnleft");
-                telemetry.addLine("gampad1.dpad_right = turnright");
-                telemetry.addLine("gampad1.dpad_down = driveforward");
+                telemetry.addLine("gampad1.dpad_up = drive forward");
+                telemetry.addLine("gampad1.dpad_left = turn left");
+                telemetry.addLine("gampad1.dpad_right = turn right");
+                telemetry.addLine("gampad1.dpad_down = drive back");
 
                 telemetry.addLine("gampad2.dpad_right = straferight");
                 telemetry.addLine("gampad2.dpad_left = strafeleft");
-                telemetry.addLine("gampad2.dpad_right = turnright");
-                telemetry.addLine("gampad1.dpad_up = armUpDown");
+                telemetry.addLine("gampad2.dpad_up = armUpDown");
 
                 telemetry.addLine("gampad1.right_bumper = close servo");
                 telemetry.addLine("gampad1.left_bumper = open servo");
